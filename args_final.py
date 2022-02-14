d755fe5ba691eeee315e637425421110c275881d
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2022-02-14 23:01:22 +0530

    Update args_final.py

--- args_final.py
+++ args_final.py
@@ -33,6 +33,9 @@ def args():
         "--nfeatures", default=17, type=int, help="Dataset(BCI3)"
     )
     parser.add_argument(
+        "--max_feat", default=5, type=int, help="Dataset(BCI3)"
+    )
+    parser.add_argument(
         "--gen", default=2, type=int, help="Dataset(BCI3)"
     )
     
