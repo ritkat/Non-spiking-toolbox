e3591c9eef3cc2f2165b37acffc83ce6a2d7c703
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2022-03-01 15:37:44 +0530

    Update baseline_final.py

--- baseline_final.py
+++ baseline_final.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
         )
 
         n_generations=10
-        l_feat=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        l_feat=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
         #args.gen=n_generations
         args.l_feat=l_feat
         args.niter=200
