3813d6857fbcf494d3c1afd1390a9597fb9d70e8
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2022-03-08 16:03:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -652,7 +652,7 @@ def createFV_individual(data_train, data_test,f_split, fs, l_feat, c_ref):
       #data = load()
                               
       
-      for i in range(0, data_2_subs_t.shape[1].shape[1]):
+      for i in range(0, data_2_subs_t.shape[1]):
           for x in range(f_split):
             rho, sigma = sm.regression.linear_model.burg(data_trial_s[x][:,i], order=2)
             '''rho2, sigma2 = sm.regression.linear_model.burg(data_trial_s2[:,i], order=2)
